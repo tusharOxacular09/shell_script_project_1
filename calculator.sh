c18d94a4c88ca54c4c46bdd4960f99e8bd516e88
tusharOxacular09 <tusharoxacular@gmail.com>
2024-04-12 06:44:53 +0530

    Percentage functionality

--- calculator.sh
+++ calculator.sh
@@ -9,7 +9,11 @@ y=$((20 - 10))
 # Divison
 z = $((100/9.8))
 
+# Percentage
+p=$((82/100))
+
 # Print the value
 echo "Addition: $x"
 echo "Subtraction: $y"
 echo "Division: $z"
+echo "Percentage: $p"
